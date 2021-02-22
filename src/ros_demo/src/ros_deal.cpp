f7019167c2c17e09e9505a0a7f6f2b048b440266
TANG-github-xy <t14726688741@163.com>
2021-02-22 14:17:27 +0800

    完成

--- src/ros_demo/src/ros_deal.cpp
+++ src/ros_demo/src/ros_deal.cpp
@@ -1,15 +1,21 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "ros_demo/info.h"
+#include "ros_demo/gpsimu.h"
 #include <string>
 #include <iostream>
 using namespace std;
 ros::Publisher chatter_pub;
 ros::Publisher GPS_IMU_state_pub;
-
+ros::Publisher GPS_IMU_DATA;
 int lenth(int advance_data, int len, const std_msgs::String::ConstPtr& msg)
 {
-	/*数据先验,计算字符串长度*/
+	/*数据先验,计算每条数据字符串长度*/
+    /********************************************/
+    //param: advance_data ,一个大于数据字符串长度的任意值
+    //param: len ,计算起始位，一般为1
+    //param:  msg,消息引用
+    /********************************************/
 	if(msg->data[0]=='$')
 		{
 			 for(int i=0; i<advance_data; i++)
@@ -27,6 +33,11 @@ int lenth(int advance_data, int len, const std_msgs::String::ConstPtr& msg)
 
 int GPS_Check(char c1,char c2, int dex)
 {
+    /*****************************************/
+    //param : c1 校验位第一位
+    //param : c2 校验为第二位
+    //param : dex 异或操作后的十进制值
+    /*****************************************/
 	int int1, int2, mask;
         if(c1 >='A' && c1<='F'){
             int1 = 10+(c1-'A');
@@ -54,6 +65,10 @@ int GPS_Check(char c1,char c2, int dex)
 
 int GPS_Data_Check(uint8_t a[],int length)//数据校验
 {
+    /*****************************************************/
+    //param: a[]消息数组的引用
+    //param: length数组的长度
+    /*****************************************************/
     int head_n=5;
     string head_str; //帧头
     string CS_str; //数据帧校验位
@@ -99,6 +114,9 @@ int GPS_Data_Check(uint8_t a[],int length)//数据校验
 
 struct GPS_IMU
 {
+    /******************************/
+    //根据具KSXT协议
+    /*****************************/
         string Header;
         string GPSTIME;
         string Longitude;
@@ -125,21 +143,20 @@ struct GPS_IMU
 
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
-	ROS_INFO("I heard: [%s]", msg->data.c_str());
+	 ROS_INFO("I heard: [%s]", msg->data.c_str());
     
-    int len=lenth(1000,1, msg);
+    int len=lenth(1000,1, msg);//计算消息长度
 
-	 uint8_t a[len];
+	 uint8_t a[len];//将ros的消息格式转化成普通数组的格式
     for(int i=0; i<len; i++)
 	{
 		a[i] = msg->data[i];
 	}
     
-    int m =GPS_Data_Check(a,len);
+    int m =GPS_Data_Check(a,len);//校验消息，去除不完整的数据
     //cout<<"较验值情况为："<<m<<"\n";
     struct GPS_IMU D;
 	if((len>230||len<190)||(m==0)){
-
 		cout<<"此数据异常不可取\n******************************************\n\n\n";
 	}
    else
@@ -237,8 +254,7 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
            }
     }
      
-     //cout<<"\n head:"<<D.Header<<"******"<<D.DD_Speed;
-
+    /*发送组合惯导的状态信息，包括消息协议，差分状态，当前服务卫星数*/
      ros_demo::info data_state;
      data_state.head = D.Header;
 	 data_state.state_1 = D.Position_state_1;
@@ -246,7 +262,29 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
      data_state.f_satellite = D.f_satellite;
      data_state.b_satellite = D.b_satellite;
      GPS_IMU_state_pub.publish(data_state);
-	 
+	 /*发送组合惯导的全部数据*/
+     ros_demo::gpsimu gps_imu;
+     gps_imu.head = D.Header;
+     gps_imu.gpstime = D.GPSTIME;
+     gps_imu.longitude = D.Longitude;
+     gps_imu.lattitude = D.Lattitude;
+     gps_imu.altitude = D.Altitude;
+     gps_imu.yaw = D.Yaw;
+     gps_imu.pitch = D.Pitch;
+     gps_imu.v_direction_angle = D.V_direction_Angle;
+     gps_imu.speed = D.speed;
+     gps_imu.roll = D.Roll;
+     gps_imu.position_state_1 = D.Position_state_1;
+     gps_imu.position_state_2 = D.Position_state_2;
+     gps_imu.f_satellite =D.f_satellite;
+     gps_imu.b_satellite = D.b_satellite;
+     gps_imu.e_Distance = D.E_Distance;
+     gps_imu.n_Distance = D.N_Distance;
+     gps_imu.d_Distance = D.D_Distance;
+     gps_imu.ed_Speed = D.ED_Speed;
+     gps_imu.nd_Speed = D.ED_Speed;
+     gps_imu.dd_Speed = D.DD_Speed;
+     GPS_IMU_DATA.publish(gps_imu);
    
 
 }
@@ -254,12 +292,13 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg)
 int main(int argc, char *argv[])
 {
 	
-	ros::init(argc, argv, "data_deel");
+	ros::init(argc, argv, "GPS_IMU");
 	ros::NodeHandle n;
 	
 	ros::Subscriber sub = n.subscribe("read", 1, chatterCallback);
 	chatter_pub = n.advertise<std_msgs::String>("/chatter", 1000);
-    GPS_IMU_state_pub = n.advertise<ros_demo::info>("/GPS_IMU_State", 1000);		
+    GPS_IMU_state_pub = n.advertise<ros_demo::info>("/GPS_IMU_State", 1000);
+    GPS_IMU_DATA = n.advertise<ros_demo::gpsimu>("GPS_IMU_DATA", 1000);	
 	ros::spin();
 
 	return 0;
